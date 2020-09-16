4d7daec4fb3bd67a679cbddbea3223fa7d9c369b
Stephen Haunts <stephen.haunts@outlook.com>
2020-09-16 11:38:35 +0100

    Check in initial example for ProtectedData

--- Building Secure Applications with Cryptography/ProtectedDataExample/Program.cs
+++ Building Secure Applications with Cryptography/ProtectedDataExample/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿/*
+MIT License
 
-namespace ProtectedDataExample
+Copyright (c) 2020
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+using System;
+using System.Security.Cryptography;
+
+namespace Pluralsight.ProtectedDataExample
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             var encrypted = Protected.Protect("Mary had a little lamb", "8wef5juy2389f4", DataProtectionScope.CurrentUser);
+             Console.WriteLine(encrypted);
+
+             var decrypted = Protected.Unprotect(encrypted, "8wef5juy2389f4", DataProtectionScope.CurrentUser);
+             Console.WriteLine(decrypted);
         }
     }
 }
---
+++ Building Secure Applications with Cryptography/ProtectedDataExample/Protected.cs
@@ -0,0 +1,71 @@
+﻿/*
+MIT License
+
+Copyright (c) 2020
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Pluralsight.ProtectedDataExample
+{
+    public class Protected
+    { 
+        public static string Protect(string stringToEncrypt, string optionalEntropy, DataProtectionScope scope)
+        {      
+            byte[] encryptedData = ProtectedData.Protect(
+                    Encoding.UTF8.GetBytes(stringToEncrypt)
+                    , optionalEntropy != null ? Encoding.UTF8.GetBytes(optionalEntropy) : null
+                    , scope);
+
+            return Convert.ToBase64String(encryptedData);
+        }
+
+        public static string Unprotect(string encryptedString, string optionalEntropy, DataProtectionScope scope)
+        {          
+            byte[] decrypted = ProtectedData.Unprotect(
+                    Convert.FromBase64String(encryptedString)
+                    , optionalEntropy != null ? Encoding.UTF8.GetBytes(optionalEntropy) : null
+                    , scope);
+
+            return Encoding.UTF8.GetString(decrypted);
+        }
+
+        public static byte[] Protect(byte[] stringToEncrypt, byte[] optionalEntropy, DataProtectionScope scope)
+        {
+            byte[] encryptedData = ProtectedData.Protect(stringToEncrypt
+                    , optionalEntropy != null ? optionalEntropy : null
+                    , scope);
+
+            return encryptedData;
+        }
+
+        public static byte[] Unprotect(byte[] encryptedString, byte[] optionalEntropy, DataProtectionScope scope)
+        {
+            byte[] decrypted = ProtectedData.Unprotect(encryptedString,
+                    optionalEntropy != null ? optionalEntropy : null,
+                    scope);
+
+            return decrypted;
+        }
+    }
+}
