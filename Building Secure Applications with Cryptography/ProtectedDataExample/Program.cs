cf8c2b014977e68fd31206394fbe5a29039417f9
Stephen Haunts <stephen.haunts@outlook.com>
2020-09-16 10:12:57 +0100

    Add project for the ProtectedData example.

---
+++ Building Secure Applications with Cryptography/ProtectedDataExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProtectedDataExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
