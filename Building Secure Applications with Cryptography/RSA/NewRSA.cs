f5e2cdb90e4aa982c87a529e7d6fd8ade9787613
Stephen Haunts <stephen.haunts@outlook.com>
2020-10-07 11:56:55 +0100

    Refactor new rsa example to make it easier to understand.

---
+++ Building Secure Applications with Cryptography/RSA/NewRSA.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Pluralsight.Asymetric
+{
+	class NewRSA
+    {
+        private RSA rsa; 
+ 
+        public NewRSA()
+        {
+            rsa = RSA.Create(2048);
+        }
+
+        public byte[] Encrypt(string dataToEncrypt)
+        {
+            return rsa.Encrypt(Encoding.UTF8.GetBytes(dataToEncrypt), RSAEncryptionPadding.OaepSHA256);
+        }
+
+        public byte[] Encrypt(byte[] dataToEncrypt)
+        {
+            return rsa.Encrypt(dataToEncrypt, RSAEncryptionPadding.OaepSHA256);
+        }
+
+        public byte[] Decrypt(byte[] dataToDecrypt)
+        {
+            return rsa.Decrypt(dataToDecrypt, RSAEncryptionPadding.OaepSHA256);
+        }
+
+        public byte[] ExportPrivateKey(int numberOfIterations, string password)
+        {
+            byte[] encryptedPrivateKey = new byte[2000];
+
+            PbeParameters keyParams = new PbeParameters(PbeEncryptionAlgorithm.Aes256Cbc, HashAlgorithmName.SHA256, numberOfIterations);  
+            var arraySpan = new Span<byte>(encryptedPrivateKey);
+            rsa.TryExportEncryptedPkcs8PrivateKey(Encoding.UTF8.GetBytes(password), keyParams, arraySpan, out _);
+
+            return encryptedPrivateKey;
+        }
+
+        public void ImportEncryptedPrivateKey(byte[] encryptedKey, string password)
+        {
+            rsa.ImportEncryptedPkcs8PrivateKey(Encoding.UTF8.GetBytes(password), encryptedKey, out _);
+        }
+
+        public byte[] ExportPublicKey()
+        {
+            return rsa.ExportRSAPublicKey();
+        }
+
+        public void ImportPublicKey(byte[] publicKey)
+        {
+            rsa.ImportRSAPublicKey(publicKey, out _);
+        }
+    }
+}
--- Building Secure Applications with Cryptography/RSA/Program.cs
+++ Building Secure Applications with Cryptography/RSA/Program.cs
@@ -37,6 +37,11 @@ namespace Pluralsight.Asymetric
             //
             //RsaWithCsp();
 
+
+            NewRSAEncryptDecrypt();
+
+            NewRSAEncryptDecryptWithKeyExport();
+
             Console.ReadLine();
         }
 
@@ -83,6 +88,55 @@ namespace Pluralsight.Asymetric
             Console.WriteLine("   Encrypted Text = " + Convert.ToBase64String(encryptedRsaParams));
             Console.WriteLine();
             Console.WriteLine("   Decrypted Text = " + Encoding.Default.GetString(decryptedRsaParams));
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        private static void NewRSAEncryptDecrypt()
+        {
+            var rsa = new NewRSA();
+            const string original = "Text to encrypt";
+
+            var encrypted = rsa.Encrypt(original);
+            var decrypted = rsa.Decrypt(encrypted);
+
+
+            Console.WriteLine("New RSA Encryption Demonstration in .NET");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("   Original Text = " + original);
+            Console.WriteLine();
+            Console.WriteLine("   Encrypted Text = " + Convert.ToBase64String(encrypted));
+            Console.WriteLine();
+            Console.WriteLine("   Decrypted Text = " + Encoding.Default.GetString(decrypted));
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        private static void NewRSAEncryptDecryptWithKeyExport()
+        {
+            var rsa = new NewRSA();
+            byte[] encryptedPrivateKey = rsa.ExportPrivateKey(100000, "iwf57yn783425y");
+            byte[] publicKey = rsa.ExportPublicKey();
+
+            const string original = "Text to encrypt";
+            var encrypted = rsa.Encrypt(original);
+
+            var rsa2 = new NewRSA();
+            rsa2.ImportPublicKey(publicKey);
+            rsa2.ImportEncryptedPrivateKey(encryptedPrivateKey, "iwf57yn783425y");
+
+             var decrypted = rsa2.Decrypt(encrypted);
+
+
+            Console.WriteLine("New RSA Encryption With Imported Key Demonstration in .NET");
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("   Original Text = " + original);
+            Console.WriteLine();
+            Console.WriteLine("   Encrypted Text = " + Convert.ToBase64String(encrypted));
+            Console.WriteLine();
+            Console.WriteLine("   Decrypted Text = " + Encoding.Default.GetString(decrypted));
             Console.WriteLine();
             Console.WriteLine();
         }
